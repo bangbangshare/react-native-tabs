26de24d08042ace7c3f5433ca7ad4ce3fc6023db
seanzhu <zhuyunxiang@hotmail.com>
2017-08-01 22:28:45 +0800

    修改样式

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-'use strict';
+ 'use strict';
 
 import React, {
     Component
@@ -9,15 +9,67 @@ import {
     View,
     Text,
     TouchableOpacity,
+    Animated,
+    Dimensions,
+    Easing
 } from 'react-native';
 
 class Tabs extends Component {
+    constructor(props) {
+        super(props);
+        this.animatedValue = new Animated.Value(0);
+        this.state = {
+            translateY: this.animatedValue.interpolate({
+                inputRange: [0, 1],
+                outputRange: [0, 100]
+            })
+        }
+    }
     onSelect(el){
         if (el.props.onSelect) {
             el.props.onSelect(el);
         } else if (this.props.onSelect) {
             this.props.onSelect(el);
         }
+    }
+    componentWillUpdate(nextProps, nextState) {
+        if(nextProps.scrolling  && nextProps.scrolling != this.props.scrolling){
+            var translateY = this.animatedValue.interpolate({
+                inputRange: [0, 1],
+                outputRange: [0, 100]
+            })
+            this.animate()
+            this.setState({
+                translateY:translateY
+            })
+        }else if(!nextProps.scrolling && nextProps.scrolling != this.props.scrolling){
+            var translateY = this.animatedValue.interpolate({
+                inputRange: [0, 1],
+                outputRange: [100, 0]
+            })
+            this.animate()
+            this.setState({
+                translateY:translateY
+            })
+        }
+    }
+
+    animate () {
+      this.animatedValue.setValue(0)
+      const createAnimation = function (value, duration, easing, delay = 50) {
+        return Animated.timing(
+          value,
+          {
+            toValue: 1,
+            duration,
+            easing,
+            delay
+          }
+        )
+      }
+      Animated.parallel([
+        createAnimation(this.animatedValue, 400, Easing.ease),     
+      ]).start()
     }
 
     render(){
@@ -30,8 +82,10 @@ class Tabs extends Component {
                 }
             });
         }
+
+        
         return (
-            <View style={[styles.tabbarView, this.props.style]}>
+            <Animated.View style={[styles.tabbarView, this.props.style, {transform:[{translateY:this.state.translateY}]}]}>
                 {React.Children.map(this.props.children.filter(c=>c),(el)=>
                     <TouchableOpacity key={el.props.name+"touch"}
                        style={[styles.iconView, this.props.iconStyle, (el.props.name || el.key) == selected ? this.props.selectedIconStyle || el.props.selectedIconStyle || {} : {} ]}
@@ -41,7 +95,7 @@ class Tabs extends Component {
                          {selected == (el.props.name || el.key) ? React.cloneElement(el, {selected: true, style: [el.props.style, this.props.selectedStyle, el.props.selectedStyle]}) : el}
                     </TouchableOpacity>
                 )}
-            </View>
+            </Animated.View>
         );
     }
 }
